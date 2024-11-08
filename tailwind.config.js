a8606d7ab1fa23207ade19e2334a473c1a0d8218
NDDC NIGERIA <ob384@live.mdx.ac.uk>
2024-11-08 20:09:51 +0100

    Course Details Page, Avatar Addition, Vue.js Introduction

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,16 @@ module.exports = {
     },
   },
   plugins: [
-    // require('@tailwindcss/forms')
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* Internet Explorer 10+ */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none', /* Safari and Chrome */
+        },
+      });
+    },
   ],
 }
