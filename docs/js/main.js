26301c74ff004fc11e5c136505a455ef66ae2a7c
NDDC NIGERIA <ob384@live.mdx.ac.uk>
2024-12-01 16:23:51 +0100

    Placeholder index update

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,6 +7,6 @@ let app = new Vue({
   },
   beforeMount(){
     fetch("https://after-mdx-backend.onrender.com/api/courses/trending").then((res)=>(res.json())).then((d) => this.trendingCourses = [...d]);
-    fetch("https://after-mdx-backend.onrender.com/api/username",{credentials: 'include',}).then((res)=>(res.json())).then((d) => {this.username = d.username; console.log(`Username is ${this.username}`)});
+    fetch("https://after-mdx-backend.onrender.com/api/username").then((res)=>(res.json())).then((d) => {this.username = d.username; console.log(`Username is ${this.username}`)});
   }
 });
