c09d9667ddea27c2c5a5821cc6bebc165e911f73
NDDC NIGERIA <ob384@live.mdx.ac.uk>
2024-12-01 14:31:50 +0100

    Sign up and username frontend update

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,8 +3,10 @@ let app = new Vue({
   data: {
     loggedIn: false,
     trendingCourses: [], 
+    username: '',
   },
   beforeMount(){
     fetch("https://after-mdx-backend.onrender.com/api/courses/trending").then((res)=>(res.json())).then((d) => this.trendingCourses = [...d]);
+    fetch("https://after-mdx-backend.onrender.com/api/username").then((res)=>(res.json())).then((d) => {this.username = d.username; console.log(`Username is ${this.username}`)});
   }
 });
