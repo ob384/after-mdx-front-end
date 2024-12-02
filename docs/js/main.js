e2c5f2e595266e8b9be81af708aa8e5c094af630
NDDC NIGERIA <ob384@live.mdx.ac.uk>
2024-12-02 09:43:43 +0100

    Index

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,6 +4,31 @@ let app = new Vue({
     loggedIn: false,
     trendingCourses: [], 
     username: '',
+    cart : [],
+  },
+  methods: {
+    addToCart(course) {
+      // Check if the course is already in the cart
+      if (course.space>0) {
+        course.space -=1
+        const inCart = this.cart.find(item => item.id === course.id);
+        if (inCart) {
+          inCart.quantity += 1
+        } else {
+          this.cart.push({ ...param, quantity: 1 });
+        }
+        sessionStorage.setItem('cart', JSON.stringify(this.cart));
+        alert(" added to cart!");
+      }
+  
+      
+  
+      // Optional: Add some user feedback
+      this.$toast.success(`${courseToAdd.name} added to cart`);
+  
+      // Optional: Persist cart to localStorage
+      localStorage.setItem('cart', JSON.stringify(this.cart));
+    }
   },
   beforeMount(){
     fetch("https://after-mdx-backend.onrender.com/api/courses/trending").then((res)=>(res.json())).then((d) => this.trendingCourses = [...d]);
